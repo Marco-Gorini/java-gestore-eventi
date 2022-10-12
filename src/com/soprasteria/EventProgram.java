f65b20fd01f061be2c7d3cfbfb48372be616f102
GORINI Marco <marco.gorini@soprasteria.com>
2022-10-12 16:28:48 +0200

    last commit

--- src/com/soprasteria/EventProgram.java
+++ src/com/soprasteria/EventProgram.java
@@ -1,26 +1,31 @@
 package com.soprasteria;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+import Exceptions.IncorrectDayException;
+import Exceptions.IncorrectTotalSeatsException;
+
 public class EventProgram {
 	private String title;
 	private List<Event> events;
 	
-	
-	
 	//Constructor
-	
-	public List<Event> getEvents() {
-		return events;
-	}
 
 	public EventProgram(String title) {
 		this.title = title;
 		this.events = new ArrayList<>();
+	}
+	
+	//Getters and Setters
+	
+	public List<Event> getEvents(){
+		return events;
 	}
 	
 	//Other Methods
@@ -73,9 +78,26 @@ public class EventProgram {
 	public void save(String path) throws IOException {
 		File filePath = new File(path);
 		FileWriter fileWriter = new FileWriter(filePath);
+		fileWriter.write(title + "\n");
 		for(Event event : events) {
-			fileWriter.write(event.toString());
+			fileWriter.write(event.title + "/" + event.getDate() + "/" + event.getTotalNumber() + "/" + event.getAlreadyBooked());
 		}
+		fileWriter.close();
+	}
+	
+	public static EventProgram charge(String path) throws IOException, NumberFormatException, IncorrectDayException, IncorrectTotalSeatsException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+		String line = br.readLine();
+		EventProgram eventProgram = new EventProgram(line);
+		while ((line = br.readLine()) != null) {
+			String dataOfEvent[] = line.split("/");
+			String stringToDate[] = dataOfEvent[1].split("-");
+			LocalDate dateEvent = LocalDate.of(Integer.parseInt(stringToDate[0]), Integer.parseInt(stringToDate[1]), Integer.parseInt(stringToDate[2]));
+			Event event = new Event(dataOfEvent[0],dateEvent,Integer.parseInt(dataOfEvent[2]));
+			eventProgram.getEvents().add(event);
+		}
+		br.close();
+		return eventProgram;
 	}
 	
 }
